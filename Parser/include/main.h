81b048d692a578df3ca43ae2b0f30bd1f9dcc23c
juangod1 <juan.godfrid@gmail.com>
2018-10-09 17:59:52 -0300

    parser

---
+++ Parser/include/main.h
@@ -0,0 +1,8 @@
+//
+// Created by juangod on 04/10/18.
+//
+
+#ifndef PROTOS_TPE_MAIN_H
+#define PROTOS_TPE_MAIN_H
+
+#endif //PROTOS_TPE_MAIN_H
---
+++ Parser/include/options.h
@@ -0,0 +1,11 @@
+//
+// Created by juangod on 08/10/18.
+//
+
+#ifndef PROTOS_TPE_OPTIONS_H
+#define PROTOS_TPE_OPTIONS_H
+
+
+void initialize_options();
+
+#endif //PROTOS_TPE_OPTIONS_H
