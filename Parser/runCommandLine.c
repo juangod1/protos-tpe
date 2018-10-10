af3afab74775f268bc65aa99edc991a3ccc60ea2
juangod1 <juan.godfrid@gmail.com>
2018-10-10 20:49:28 -0300

    types

--- Parser/runCommandLine.c
+++ Parser/runCommandLine.c
@@ -1,5 +1,7 @@
 #include "include/runCommandLine.h"
 #include "include/parityByte.h"
+#include <sys/types.h>
+
 //https://stackoverflow.com/questions/28507950/calling-ls-with-execv
 //https://jineshkj.wordpress.com/2006/12/22/how-to-capture-stdin-stdout-and-stderr-of-child-program/
 
