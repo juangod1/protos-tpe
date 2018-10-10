ded87c4d3ceb3bda76430f2d4db1e9dae73a5b24
juangod1 <juan.godfrid@gmail.com>
2018-10-10 17:20:20 -0300

    wip connection origin mua

--- Proxy/include/main.h
+++ Proxy/include/main.h
@@ -1,11 +1,18 @@
 #ifndef PROTOS_TPE_MAIN_H
 #define PROTOS_TPE_MAIN_H
 
-#define MUAPORT 1110
+typedef const int file_descriptor;
 
-void setup_MUA_socket();
+#define MUA_PORT 1110
+#define ORIGIN_PORT  110
+#define SELECT_FILE_DESCRIPTOR_AMOUNT 2
+
+file_descriptor setup_MUA_socket();
+file_descriptor setup_origin_socket(char * origin_address);
 void error();
-void run_server();
+void run_server(file_descriptor MUA_sock, file_descriptor Origin_sock);
+int max(int a, int b);
+int findMax(int * a, int size);
 
 int proxy_main();
 #endif //PROTOS_TPE_MAIN_H
--- Proxy/main.c
+++ Proxy/main.c
@@ -1,6 +1,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <memory.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <sys/param.h>
 #include "include/main.h"
 #include "../Shared/include/executionValidator.h"
 #include "include/proxyParse.h"
@@ -9,69 +12,102 @@ int main(int argc, char ** argv)
 {
     int response = proxy_parse(argc,argv);
 
-    if(response==ERROR)
-    {
-        printf("Program execution stopped.\n");
-    }
-    if(response==STANDARD)
-    {
-        execute_options();
-        proxy_main();
-    }
-    if(response==HELP)
-    {
-        help();
-    }
-    if(response==VERSION)
-    {
-        version();
-    }
-    return response;
-}
+    file_descriptor MUA_sock = setup_MUA_socket();
+    file_descriptor Origin_sock = setup_origin_socket("127.0.0.1");
 
-int proxy_main()
-{
-    printf("Starting Program...\n");
-    setup_MUA_socket();
+    //run_server(MUA_sock, Origin_sock);
 
     return 0;
 }
 
-void setup_MUA_socket()
+/*void run_server(file_descriptor MUA_sock, file_descriptor Origin_sock)
 {
+    fd_set file_descriptors;
+    struct timeval tv;
+    int select_ret;
+
+    FD_ZERO(&file_descriptors);
+    FD_SET(MUA_sock,&file_descriptors);
+    FD_SET(MUA_sock,&file_descriptors);
+
+    file_descriptors[0].fd = MUA_sock;
+    file_descriptors[1].fd = Origin_sock;
+
+    int max_fd = findMax((int*)file_descriptors,SELECT_FILE_DESCRIPTOR_AMOUNT);
+
+    for(;;){
+        select_ret = pselect(max_fd,);
+
+        if(select_ret == -1)
+        {
+            perror("Poll error.");
+            error();
+        }
+    }
+}*/
+
+file_descriptor setup_origin_socket(char * origin_address){
     struct sockaddr_in address;
     memset(&address, 0, sizeof(address));
-    address.sin_port = htons(MUAPORT);
+    address.sin_port = htons(ORIGIN_PORT);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    const int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    file_descriptor sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    if(server < 0)
+    if(sock < 0)
     {
         perror("Unable to create socket.\n");
         error();
     }
-    else
+
+    if (connect(sock, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
-        printf("Listening on port %d...\n",MUAPORT);
+        perror("Unable to connect to origin server.");
+        error();
+    }
+    else{
+        printf("Connected to server %s",origin_address);
     }
 
-    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+    return sock;
+}
 
-    if(bind(server, (struct sockaddr*) &address, sizeof(address)) < 0)
+file_descriptor setup_MUA_socket()
+{
+    struct sockaddr_in address;
+    memset(&address, 0, sizeof(address));
+    address.sin_port = htons(MUA_PORT);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    file_descriptor sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if(sock < 0)
+    {
+        perror("Unable to create socket.");
+        error();
+    }
+    else
+    {
+        printf("Listening on port %d...",MUA_PORT);
+    }
+
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+
+    if(bind(sock, (struct sockaddr*) &address, sizeof(address)) < 0)
     {
         perror("Unable to bind socket.\n");
         error();
     }
 
-    if (listen(server, 20) < 0)
+    if (listen(sock, 20) < 0)
     {
         perror("Unable to listen.\n");
         error();
     }
 
-    run_server();
+    return sock;
 }
 
 void error()
@@ -79,7 +115,13 @@ void error()
 
 }
 
-void run_server()
-{
+int findMax(int * a, int size){
+    int i,m=0;
+    for(i=0;i<size;i++)
+        m=max(m,a[i]);
+    return m;
+}
 
+int max(int a, int b){
+    return a>b?a:b;
 }
